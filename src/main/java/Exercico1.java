206962fa4b3ca4f34062cbecfd7f16a5347178dc
BatataPo <100451624+BatataPo@users.noreply.github.com>
2022-02-25 22:27:52 -0300

    R

---
+++ src/main/java/Exercico1.java
@@ -0,0 +1,15 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Exercico1 {
+    public static void main(String[] args) {
+        int zumbi1 = 120;
+        int zumbi2 = 240;
+        int zumbi3 = 320;
+
+        int total = 0;
+        total = zumbi1+zumbi2+zumbi3;
+
+        System.out.println("Total de zumbis:"+total);
+        System.out.println("Media: "+ total/3);
+    }
+}
